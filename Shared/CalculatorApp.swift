7c25c0e4730db9b992a0cc3a2b4749abd01a355b
Joao Luis <joaoluisramos45@gmail.com>
2023-03-26 23:00:03 -0300

    Initial Commit

---
+++ Shared/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Shared
+//
+//  Created by Joao Luis on 26/03/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

490b64f00bea82c149170bc1ed20c67685cf98ab
João Luís <joaoluisramos45@gmail.com>
2023-03-27 09:51:55 -0300

    new features
    sum , subtrate, multiplicate, divison , percent, sinal

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,19 +9,79 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var result = 0
+    @State var values = "0"
+    @State var previous = 0.0
+    @State var result = 0.0
+    @State var decimal = 0.0
+    
+    @State var operation = 0
+    @State var previousOperation = 0
+    
+    
+    func removeZerosFromEnd(value: Double) -> String{
+        let f = NumberFormatter()
+        let number = NSNumber(value: value)
+        f.minimumFractionDigits = 0
+        f.maximumFractionDigits = 16
+        return f.string(from: number) ?? ""
+    }
+    
+    
+    func process(digit: Int){
+        if operation > 0 {
+            result = 0
+            previousOperation = operation
+            operation = -1
+        }
+        
+        if decimal > 0.0 {
+            result = result + Double(truncating: NSNumber(value: (Double(digit) / decimal)))
+            decimal = decimal * 10
+            values = "\(values)\(digit)"
+        }else{
+        result = (result * 10) + Double(digit)
+        values = removeZerosFromEnd(value: result)
+        }
+    }
+    
+    func calculate(){
+        if previousOperation == 1 { //soma
+            result = previous + result
+            previousOperation = 0
+        }
+        
+        else if previousOperation == 2 { //subtração
+            result = previous - result
+            previousOperation = 0
+        }
+        else if previousOperation == 3{ //multiplicação
+            result = previous * result
+            previousOperation = 0
+        }
+        else if previousOperation == 4{//divisão
+            result = previous / result
+            previousOperation = 0
+        }
+        previous = result
+        decimal = 0.0
+        values = removeZerosFromEnd(value: result)
+        
+    }
+    
+    func reset(){
+        previous = 0
+        operation = 0
+        previousOperation = 0
+        result = 0
+        decimal = 0.0
+        values = "0"
+    }
     
     var body: some View {
         VStack(alignment: .trailing, spacing: 0){
-            
-            Text("\(String(result).count)")
-                .foregroundColor(Color.red)
-            
-            
             Spacer()
-            
             HStack{
-                Text(String(result))
+                Text(values)
             }.padding()
                 .lineLimit(1)
                 .font(.system(size: CGFloat(80 / Int((Double(String(result).count + 9) / 10.0)))))
@@ -29,24 +89,27 @@ struct ContentView: View {
                 .frame(maxWidth: .infinity)
                 .fixedSize(horizontal: true, vertical: false)
             
-            HStack{
+            HStack(spacing: 0){
                 Button("AC"){
-                    result = 0
+                    reset()
                 }.padding()
                     .frame(maxWidth: .infinity)
                     
                 Button("+/-"){
-                    
+                    result = result * -1
+                    calculate()
                 }.padding()
                     .frame(maxWidth: .infinity)
                    
                 Button("%"){
-                    
+                    result = result / 100
+                    calculate()
                 }.padding()
                     .frame(maxWidth: .infinity)
                     
-                Button("//"){
-                    
+                Button("/"){
+                    calculate()
+                    operation = 4
                 }.padding(.vertical, 40)
                 .frame(maxWidth: .infinity)
                 .background(Color.orange)
@@ -54,72 +117,75 @@ struct ContentView: View {
             }.foregroundColor(Color.white)
                 .background(Color.gray)
             
-            HStack{
+            HStack(spacing: 0){
                 Button("7"){
-                    result = (result * 10) + 7
+                    process(digit: 7)
                 }.padding()
                     .frame(maxWidth: .infinity)
                     
                 Button("8"){
-                    result = (result * 10) + 8
+                    process(digit: 8)
                 }.padding()
                     .frame(maxWidth: .infinity)
                    
                 Button("9"){
-                    result = (result * 10) + 9
+                    process(digit: 9)
                 }.padding()
                     .frame(maxWidth: .infinity)
                     
                 Button("X"){
-                    
+                    calculate()
+                    operation = 3
                 }.padding(.vertical, 40)
                 .frame(maxWidth: .infinity)
                 .background(Color.orange)
                     
             }.foregroundColor(Color.white)
             
-            HStack{
+            HStack(spacing: 0){
                 Button("4"){
-                    result = (result * 10) + 2
+                    process(digit: 4)
                 }.padding()
                     .frame(maxWidth: .infinity)
                     
                 Button("5"){
-                    result = (result * 10) + 5
+                    process(digit: 5)
                 }.padding()
                     .frame(maxWidth: .infinity)
                    
                 Button("6"){
-                    result = (result * 10) + 6
+                    process(digit: 6)
                 }.padding()
                     .frame(maxWidth: .infinity)
                     
                 Button("-"){
-                    
+                    calculate()
+                    operation = 2
                 }.padding(.vertical, 40)
                 .frame(maxWidth: .infinity)
                 .background(Color.orange)
                     
             }.foregroundColor(Color.white)
             
-            HStack{
+            HStack(spacing: 0){
                 Button("1"){
-                    result = (result * 10) + 1
+                    process(digit: 1)
                 }.padding()
                     .frame(maxWidth: .infinity)
                     
                 Button("2"){
-                    result = (result * 10) + 2
+                    process(digit: 2)
                 }.padding()
                     .frame(maxWidth: .infinity)
                    
                 Button("3"){
-                    result = (result * 10) + 3
+                    process(digit: 3)
                 }.padding()
                     .frame(maxWidth: .infinity)
                     
                 Button("+"){
-                    
+                    calculate()
+                    operation = 1
                 }.padding(.vertical, 40)
                 .frame(maxWidth: .infinity)
                 .background(Color.orange)
@@ -127,19 +193,24 @@ struct ContentView: View {
             }.foregroundColor(Color.white)
             
             GeometryReader{ geometry in
-            HStack{
+                HStack(spacing: 0){
                 Button("0"){
-                    result = (result * 10) + 0
+                    process(digit: 0)
                 }.padding()
                     .frame(minWidth: geometry.size.width / 2)
                     
-                Button(","){
-
+                Button("."){
+                    if decimal == 0.0{
+                        decimal = 10.0
+                        values = values + "."
+                    }
                 }.padding()
                     .frame(maxWidth: .infinity)
                    
                 Button("="){
-                    
+                    calculate()
+                    previousOperation = 999
+                    operation = 999
                 }.padding(.vertical, 40)
                     .frame(maxWidth: .infinity)
                     .background(Color.orange)
